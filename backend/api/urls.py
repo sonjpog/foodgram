a8f3ad7ad3844c5e306feef90c34fb12e1ce5eee
Sofia Pogosian <sonyapgsn@gmail.com>
2025-03-11 15:32:32 +0300

    urls update

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -16,7 +16,7 @@ router.register('users', CustomUserViewSet, 'users')
 
 urlpatterns = (
     path('', include(router.urls)),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls.authtoken')),
     path('recipes/<int:pk>/short-url/', short_url, name='short_url'),
 )
